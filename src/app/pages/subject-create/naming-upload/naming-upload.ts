6d8eb5aea9c4e8a7fce62121a7372dceb8a4027d
Ayomikun Ewuoso <ewuoso03@gmail.com>
2025-08-21 14:47:51 +0100

    added top section of subject creation

--- src/app/pages/subject-create/naming-upload/naming-upload.ts
+++ src/app/pages/subject-create/naming-upload/naming-upload.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../shared/components/header/header';
+import { Router } from '@angular/router';
+import { CreationStepTab } from '../creation-step-tab/creation-step-tab';
 
 @Component({
   selector: 'app-naming-upload',
-  imports: [],
+  imports: [Header, CreationStepTab],
   templateUrl: './naming-upload.html',
   styleUrl: './naming-upload.css'
 })
 export class NamingUpload {
-
+  
 }
