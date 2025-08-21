81ca2e28e65a9b3aec8ec1f6565bebe97d1cbb3a
Ayomikun Ewuoso <ewuoso03@gmail.com>
2025-08-21 12:48:13 +0100

    worked on dashboard with subjects and added prussian-blue colour variation

---
+++ src/app/core/models/subject-model.ts
@@ -0,0 +1,7 @@
+export type SubjectModel = {
+  id: string;
+  name: string;
+  createdAt: Date;
+  status: string;
+  completion: number; // Percentage from 0 to 100
+}
--- src/app/pages/dashboard/subjects/subjects.ts
+++ src/app/pages/dashboard/subjects/subjects.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { Header } from '../../../shared/components/header/header';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SubjectModel } from '../../../core/models/subject-model';
 
 @Component({
   selector: 'app-subjects',
-  imports: [Header, RouterLink],
+  imports: [Header, RouterLink, CommonModule],
   templateUrl: './subjects.html',
   styleUrl: './subjects.css'
 })
 export class Subjects {
-  subjects = [];
+  subjects: SubjectModel[] = [
+    {
+      "id": "sj2nd-2dkap-uamds",
+      "name": "Mathematics",
+      "createdAt": new Date("2023-10-01T12:00:00Z"),
+      "status": "In Progress",
+      "completion": 30,
+    },
+    {
+      "id": "sj2nd-2dkap-uamds",
+      "name": "Science",
+      "createdAt": new Date("2023-10-01T12:00:00Z"),
+      "status": "Completed",
+      "completion": 100,
+    },
+    {
+      "id": "sj2nd-2dkap-uamds",
+      "name": "History",
+      "createdAt": new Date("2023-10-01T12:00:00Z"),
+      "status": "Not Started",
+      "completion": 0,
+    },
+    {
+      "id": "sj2nd-2dkap-uamds",
+      "name": "Literature",
+      "createdAt": new Date("2023-10-01T12:00:00Z"),
+      "status": "In Progress",
+      "completion": 50,
+    }
+  ];
 }
