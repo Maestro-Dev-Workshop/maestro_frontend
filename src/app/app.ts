0fc5efa39be3c4c171c2c96cadeb4ef6bde4baa0
Ayomikun Ewuoso <ewuoso03@gmail.com>
2025-09-11 13:11:26 +0100

    fixed auth interceptor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
