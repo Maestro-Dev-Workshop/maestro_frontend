2b1598e128159d43ed179b5538a85beef243ec23
Ayomikun Ewuoso <78974837+Blaze-master@users.noreply.github.com>
2025-11-29 01:26:42 +0100

    Update production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://maestro-backend-staging.onrender.com/api', // deployed API
+  apiUrl: 'http://maestrobackend-production.up.railway.app/api', // deployed API
   beta: false
 };
