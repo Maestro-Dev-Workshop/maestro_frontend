3e4708c202c635ada4578465f2668a3d31789bd9
Ayomikun Ewuoso <ewuoso03@gmail.com>
2025-12-30 15:45:12 +0100

    bug fixes and landing page reversion

---
+++ src/app/pages/lesson/cells/chart-cell/chart-cell.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component, ElementRef, input, ViewChild } from '@angular/core';
+import * as Plotly from 'plotly.js-dist-min';
+
+@Component({
+  selector: 'app-chart-cell',
+  imports: [],
+  templateUrl: './chart-cell.html',
+  styleUrl: './chart-cell.css',
+})
+export class ChartCell implements AfterViewInit {
+  data = input<any>();
+  @ViewChild('plotContainer', { static: true })
+  plotContainer!: ElementRef;
+
+  ngAfterViewInit() {
+    const fig = JSON.parse(this.data().content);
+    Plotly.newPlot(
+      this.plotContainer.nativeElement,
+      fig.data,
+      fig.layout,
+      { responsive: true }
+    );
+    window.addEventListener('resize', () => {
+      Plotly.Plots.resize(this.plotContainer.nativeElement);
+    });
+  }
+}
