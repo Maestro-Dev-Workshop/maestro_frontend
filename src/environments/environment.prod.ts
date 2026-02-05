38b1777e8e7201a9f4df507310e2fcc45ea8c866
Ayomikun Ewuoso <78974837+Blaze-master@users.noreply.github.com>
2026-02-05 17:15:48 +0100

    Add googleClientId to beta environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://maestrobackend-beta.up.railway.app/api', // deployed API
-  beta: true
+  beta: true,
+  googleClientId:'946365756623-q7sc6fc039ihasq98t32jc2ie09q4hti.apps.googleusercontent.com',
 };
