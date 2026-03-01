c44402ef0b0e847939ae0f8336f3e20de6457a06
Ayomikun Ewuoso <ewuoso03@gmail.com>
2026-03-01 10:14:06 +0100

    fixed sign up redirection and updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://maestro-backend-staging.onrender.com/api', // deployed API
-  beta: false
+  type: 'beta',
+  apiUrl: 'https://maestrobackend-beta.up.railway.app/api', // local dev API
+  googleClientId:'946365756623-q7sc6fc039ihasq98t32jc2ie09q4hti.apps.googleusercontent.com',
 };
