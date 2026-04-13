cd0360b9dc44645678af1dd2476d4076fa80d1c9
Ayomikun Ewuoso <78974837+Blaze-master@users.noreply.github.com>
2026-04-13 09:24:28 +0100

    Update GA Measurement ID in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   type: 'prod',
   apiUrl: 'https://maestrobackend-production.up.railway.app/api', // local dev API
   googleClientId:'946365756623-q7sc6fc039ihasq98t32jc2ie09q4hti.apps.googleusercontent.com',
-  gaMeasurementId: ''
+  gaMeasurementId: 'G-7BTR6LTYXK'
 };
