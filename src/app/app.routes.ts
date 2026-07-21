d3a9fe0e94a9ab6c697a81d631c258542bc13e34
MAdebawojo <adebawojomosope@gmail.com>
2026-07-21 18:37:23 +0100

    auth bg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,14 +25,14 @@ export const routes: Routes = [
   // Lesson (protected)
   { path: 'lesson/:subjectId', loadComponent: () => import('./pages/lesson/lesson-page/lesson-page').then(m => m.LessonPage), canActivate: [authGuard] },
 
-  // Not Found
-  { path: '**', loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound) },
-
-
   // UI Overhaul Pages (All paths and imports are prefixed with `v2/`)
   // Auth
   // { path: 'v2/signup', loadComponent: () => import('./v2/pages/auth/signup/signup').then(m => m.Signup) },
-  // { path: 'v2/login', loadComponent: () => import('./v2/pages/auth/login/login').then(m => m.Login) },
+  { path: 'v2/login', loadComponent: () => import('./v2/pages/auth/login/login').then(m => m.Login) },
   // { path: 'v2/verify-email', loadComponent: () => import('./v2/pages/auth/verify-email/verify-email').then(m => m.VerifyEmail) },
   // { path: 'v2/check-email', loadComponent: () => import('./v2/pages/auth/check-email/check-email').then(m => m.CheckEmail) },
+
+  // Not Found
+  { path: '**', loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound) },
+
 ];
